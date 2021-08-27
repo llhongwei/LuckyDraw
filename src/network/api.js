e659149dbc78baef3be90318b3adbf6f119704e5
liumingxun <18945504808@163.com>
2021-08-27 13:00:02 +0800

    返回值示例

--- src/network/api.js
+++ src/network/api.js
@@ -15,7 +15,12 @@ function getDrawList() {
     method: 'get'
   })
 }
-
+//返回值示例
+/*[ { id: 0,
+  name: '坐垫',
+  img:
+    'https://sf1-ttcdn-tos.pstatp.com/obj/larkcloud-file-storage/baas/qc6nzx/c54f6bafe2e5c1c3_1630037785447.png' }
+]*/
 
 
 export { getDrawResult, getDrawList }
